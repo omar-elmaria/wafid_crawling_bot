0cddb9f0ab143c34eaecb0346a78eebfffbf09ce
Omar ElMaria <omar.elmaria@deliveryhero.com>
2023-08-05 18:14:19 +0200

    add logs, modularize code, and add driver quit upon error

--- wafid_bot_selenium.py
+++ wafid_bot_selenium.py
@@ -1,4 +1,6 @@
+# Import packages
 import asyncio
+import logging
 import os
 import time
 import warnings
@@ -17,6 +19,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 from telegram import Bot
 from webdriver_manager.chrome import ChromeDriverManager
 
+logging.basicConfig(
+    level="INFO",
+    filename="wafid_bot_logs.log",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    format="%(levelname)s - %(asctime)s - %(message)s",
+)
+
 warnings.filterwarnings(action="ignore")
 
 # Load environment variables
@@ -31,7 +40,7 @@ chrome_options.add_argument("start-maximized") # Required for a maximized Viewpo
 chrome_options.add_experimental_option('excludeSwitches', ['enable-logging', 'enable-automation', 'disable-popup-blocking']) # Disable pop-ups to speed up browsing
 chrome_options.add_experimental_option("detach", True) # Keeps the Chrome window open after all the Selenium commands/operations are performed 
 chrome_options.add_experimental_option('prefs', {'intl.accept_languages': 'en,en_US'}) # Operate Chrome using English as the main language
-# chrome_options.add_argument("--headless=new") # Operate Selenium in headless mode
+chrome_options.add_argument("--headless=new") # Operate Selenium in headless mode
 chrome_options.add_argument('--no-sandbox') # Disables the sandbox for all process types that are normally sandboxed. Meant to be used as a browser-level switch for testing purposes only
 chrome_options.add_argument('--disable-gpu') # An additional Selenium setting for headless to work properly, although for newer Selenium versions, it's not needed anymore
 chrome_options.add_argument("enable-features=NetworkServiceInProcess") # Combats the renderer timeout problem
@@ -43,8 +52,9 @@ chrome_options.add_argument("--window-size=1920x1080") # Set the Chrome window s
 base_url = "https://wafid.com/medical-status-search/"
 slip_number_list_len = 10
 
-# Global inputs (2): Get the list of slip numbers from the Google Sheet --> https://docs.google.com/spreadsheets/d/1F2F2yWmvMebUG1rtppzt1Z9RZ9bOSHwu2VjXzk4XmC8/edit?pli=1#gid=0
+###-----------------------------###-----------------------------###
 
+# Global inputs (2): Get the list of slip numbers from the Google Sheet --> https://docs.google.com/spreadsheets/d/1F2F2yWmvMebUG1rtppzt1Z9RZ9bOSHwu2VjXzk4XmC8/edit?pli=1#gid=0
 # Replace 'your_spreadsheet_key' with the key of your Google Sheets document.
 # You can find the key in the URL of your spreadsheet: 'https://docs.google.com/spreadsheets/d/your_spreadsheet_key/edit'
 SPREADSHEET_KEY = '1F2F2yWmvMebUG1rtppzt1Z9RZ9bOSHwu2VjXzk4XmC8'
@@ -65,16 +75,22 @@ worksheet = spreadsheet.get_worksheet(index=0)
 # Get all values from the worksheet
 df_slip_numbers = pd.DataFrame(worksheet.get_all_records(empty2zero=False, default_blank=None))
 
+# Create a list of slip numbers from the provided slip number
 slip_numbers_list = []
 starting_slip_number = int(df_slip_numbers["slip_number"][0])
 for i in range(1, slip_number_list_len + 1):
     slip_numbers_list.append(starting_slip_number)
     starting_slip_number += 1
 
+###-----------------------------###-----------------------------###
+
 # Global inputs (3): Telegram bot
 wafid_bot_token = os.getenv("WAFID_BOT_TOKEN")
 wafid_chat_id = os.getenv("WAFID_BOT_CHAT_ID")
+errors_bot_token = os.getenv("ERRORS_BOT_TOKEN")
+errors_bot_chat_id = os.getenv("ERRORS_BOT_CHAT_ID")
 wafid_bot_obj = Bot(token=wafid_bot_token)
+bot_errors = Bot(token=errors_bot_token)
 
 # Function to send Telegram message
 async def send_telegram_message(bot, chat_id, message):
@@ -83,22 +99,13 @@ async def send_telegram_message(bot, chat_id, message):
 # Define the current event loop to manage and execute async tasks such as coroutines and callbacks
 loop = asyncio.get_event_loop()
 
-# Instantiate the web driver and set the implicit waiting time to be 60 seconds
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
-driver.implicitly_wait(60)
-
-# Navigate to the website
-driver.get(base_url)
-
-# Click on the "Wafid Slip Number" radio button
-# driver.find_element(by=By.XPATH, value="//input[@id='id_search_variant_1']").click()
-driver.execute_script("document.getElementById('id_search_variant_1').click()")
-
-# Wait until the "GCC Slip NO" selector appears and enter the first slip number
-WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//input[@id='id_gcc_slip_no']")))
+###-----------------------------###-----------------------------###
 
 # Define a function to enter the GCC slip number and click on the "Check" button
-def gcc_slip_number_checker(slip_number):
+def gcc_enter_slip_number_func(driver, slip_number):
+    """
+    A function to enter the GCC slip number and click on the "Check" button
+    """
     for idx in range(15):
         # Declare a waiting time based on the iteration number
         if idx + 1 <= 5:
@@ -115,6 +122,8 @@ def gcc_slip_number_checker(slip_number):
         print(f"captcha_msg of gcc_slipe_number_checker iteration {idx + 1}: {captcha_msg}")
         print(f"gcc_field_content of gcc_slipe_number_checker iteration {idx + 1}: {gcc_field_content}")
 
+        # If the captcha_msg is empty and gcc_field_content contains a number, this means that the form was submitted successfully and we can break out of the loop
+        # Otherwise, we need to clear the field and try inputting the form number again
         if captcha_msg is None and gcc_field_content is not None:
             break
         else:
@@ -124,12 +133,19 @@ def gcc_slip_number_checker(slip_number):
             driver.find_element(by=By.XPATH, value="//input[@id='id_gcc_slip_no']").send_keys(slip_number)
             time.sleep(wait_time)
 
-            # If the previous command does not produce an exception, this means that the captcha message appeared, so we need to click on the "Check" button again. Otherwise, break out of the loop
+            # Click on the check button
             driver.execute_script("document.getElementById('med-status-form-submit').click()")
     return
 
-# If this message "Error verifying reCAPTCHA, please try again" pops up, then click on the check button again, up to five times
-for idx, slip in enumerate(slip_numbers_list):
+# Define a function to extract the medical center and send a Telegram notification
+def extract_medical_center(driver, idx, slip):
+    """
+    This is a function that extracts the medical center and sends a Telegram notification after the slip number has been successfully submitted.
+    Parameters of the function:
+    - driver: Chrome web driver
+    - idx: Index of the current slip number in the slip_numbers_list
+    - slip: Current slip number
+    """
     # Extract the HTML source code of the page with BeautifulSoup
     soup2 = BeautifulSoup(markup=driver.page_source, features="html.parser")
 
@@ -140,13 +156,19 @@ for idx, slip in enumerate(slip_numbers_list):
     status_message = soup2.select_one(selector="input[name='traveled_country__name'], div.header, input.g-recaptcha+p")
     print(f"status_message: {status_message}")
 
-    # Wait until the desired data appears
-    gcc_slip_number_checker(slip_number=slip)
+    # Invoke the "gcc_enter_slip_number_func" function
+    gcc_enter_slip_number_func(driver=driver, slip_number=slip)
     
+    # Download the HTML content of the page and extract the status message again
     soup3 = BeautifulSoup(markup=driver.page_source, features="html.parser")
     status_message2 = soup3.select_one(selector="input[name='traveled_country__name'], div.header, input.g-recaptcha+p")
+    
+    # The result could either be "Records not found" pr "Medical Center found". Either way, send a Telegram message
     if status_message2.get_text(strip=True) == "Records not found":
+        # Print a message saying that there was no record found for this slip number
         print(f"No records found for slip number {slip}")
+
+        # Send a Telegram message saying that there was no record found for this slip number
         loop.run_until_complete(send_telegram_message(bot=wafid_bot_obj, chat_id=wafid_chat_id, message=f"No records found for slip number {slip}"))
     if status_message2.get_attribute_list("value")[0] is not None:
         # Extract the fields of interest
@@ -155,21 +177,26 @@ for idx, slip in enumerate(slip_numbers_list):
             "country": soup3.select_one(selector="input[name='traveled_country__name']").get_attribute_list("value")[0],
             "medical_center": soup3.select_one(selector="input[name='medical_center']").get_attribute_list("value")[0]
         }
+
+        # Print the output
         print(output_dict)
+
+        # Send a Telegram message saying that a record for that slip number was found
         loop.run_until_complete(send_telegram_message(
             bot=wafid_bot_obj,
             chat_id=wafid_chat_id,
             message=f"Records were found for slip number {slip}. Info --> {output_dict}"
         ))
 
+    # If we are at the last iteration, print a message saying that we have reached the end of the iterations and exit the function. Otherwise, enter another slip number and search for a record
     if idx + 1 == len(slip_numbers_list):
         print("End of iterations. Breaking out of the loop")
-        break
+        return
     else:
         # Clear the text field containing the slip number
         driver.find_element(by=By.XPATH, value="//input[@id='id_gcc_slip_no']").clear()
 
-        # Wait 1 second before entering the slip number
+        # Wait 2.5 seconds before entering the slip number
         time.sleep(2.5)
 
         # Enter a the slip number after the current iteration
@@ -184,5 +211,38 @@ for idx, slip in enumerate(slip_numbers_list):
         # Wait until the check button becomes clickable
         WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//button[@id='med-status-form-submit']")))
 
-# Quit the web driver to save memory
-driver.quit()
+def execute_all(driver):
+    """
+    A function to execute the functions defined above
+    """
+    # Navigate to the website
+    driver.get(base_url)
+
+    # Click on the "Wafid Slip Number" radio button
+    # driver.find_element(by=By.XPATH, value="//input[@id='id_search_variant_1']").click()
+    driver.execute_script("document.getElementById('id_search_variant_1').click()")
+
+    # Wait until the "GCC Slip NO" selector appears and enter the first slip number
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//input[@id='id_gcc_slip_no']")))
+
+
+    for idx, slip in enumerate(slip_numbers_list):
+        extract_medical_center(driver=driver, idx=idx, slip=slip)
+
+    # Quit the web driver to save memory
+    driver.quit()
+
+if __name__ == "__main__":
+    try:
+        # Instantiate the web driver and set the implicit waiting time to be 60 seconds
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+        driver.implicitly_wait(60)
+
+        execute_all(driver=driver)
+    except Exception as e:
+        # Quit the driver to not take up memory
+        driver.quit()
+
+        # Send a message to the Telegram bot saying that an error occurred
+        logging.exception(f"An error occurred while crawling the wafid bot: {e}")
+        loop.run_until_complete(send_telegram_message(bot=bot_errors, chat_id=errors_bot_chat_id, message=f"An error occurred while crawling the wafid bot: {e}"))
